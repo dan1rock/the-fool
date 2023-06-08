b551c87fe8dd7aeeedf2ddf0b707866851be6aec
Danylo Holets <goletsdan@gmail.com>
2023-06-08 16:13:54 +0300

    Implemented board and deck rendering in game
    Added Board() class with createBoard() method which creates and renders board. Added rank and shirt icons. Now you can start a game and see rendered deck with trump

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 'use strict';
 
 require('./scss/index.scss');
+const { startNewGame } = require('./start.js');
+
+const newGameButton = '.startPage__newGameButton';
+document.querySelector(newGameButton).addEventListener('click', startNewGame);
